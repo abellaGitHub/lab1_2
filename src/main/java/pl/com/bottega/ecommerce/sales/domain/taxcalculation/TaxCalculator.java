8ee502f9fc89095b6d0a977660d779ff571b75f4
bella.aleksander <194912@edu.p.lodz.pl>
2018-01-13 16:59:55 +0100

    DefaultTaxCalculator class added and 'issuance' method in BookKeeper class was changed to use TaxCalculator object

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/taxcalculation/TaxCalculator.java
+++
@@ -1,8 +0,0 @@
-package pl.com.bottega.ecommerce.sales.domain.taxcalculation;
-
-import pl.com.bottega.ecommerce.sales.domain.invoicing.Tax;
-
-public interface TaxCalculator {
-
-    public Tax calculateTax();
-}
